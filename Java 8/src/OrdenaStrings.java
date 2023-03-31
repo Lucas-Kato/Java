edb4a5e58adfe3a19e12735b6323058744a4382c
Lucas Kato <lucaskato550@gmail.com>
2023-03-31 16:35:37 -0300

    .

--- Java 8/src/OrdenaStrings.java
+++ Java 8/src/OrdenaStrings.java
@@ -3,6 +3,7 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class OrdenaStrings {
     public static void main(String[] args) {
@@ -12,29 +13,47 @@ public class OrdenaStrings {
         palavras.add("Editora casa do codigo");
         palavras.add("Caelum");
 
-        Comparator<String> comparador = new ComparadorPorTamanho();
         // Collections.sort(palavras, comparador);
+        palavras.sort((s1, s2) -> {
+            if (s1.length() < s2.length())
+                return -1;
+            if (s1.length() > s2.length())
+                return 1;
+            return 0;
+        });
+
+        palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
+        
+
+        palavras.sort(Comparator.comparing(s -> s.length()));
+        palavras.sort(Comparator.comparing(String::length));
+
+        Function<String, Integer> funcao = new Function<String,Integer>() {
+            @Override
+            public Integer apply(String s) {
+                return s.length();
+            }
+            
+        };
+        Function<String, Integer> funcao2 = String::length;
+        Function<String, Integer> funcao3 = s -> s.length();
+
+
+        Comparator<String> comparador = Comparator.comparing(s -> s.length());
         palavras.sort(comparador);
 
         System.out.println(palavras);
 
         // for (String p : palavras) {
-        //     System.out.println(p);
+        // System.out.println(p);
         // }
 
-        Consumer<String> consumidor = new ImprimeNaLinha();
-        palavras.forEach(consumidor);
+        Consumer<String> impressor = System.out::println;
+        Consumer<String> impressor2 = s -> System.out.println(s);
+        palavras.forEach(impressor);
 
+        palavras.forEach(s -> System.out.println(s));
     }
-}
-
-class ImprimeNaLinha implements Consumer<String> {
-
-    @Override
-    public void accept(String s) {
-        System.out.println(s);
-    }
-
 }
 
 class ComparadorPorTamanho implements Comparator<String> {
